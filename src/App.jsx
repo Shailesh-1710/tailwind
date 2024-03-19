d3d35df61bb2f2a1fb5b3bfb1af241b0b2730401
Shailesh Patel <shailupatel17@gmail.com>
2024-03-20 00:00:15 +1000

    Tailwind presets and Navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import { useState } from "react";
-import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold ">Hello world!</h1>
-    </>
-  );
+	return (
+		<>
+			{/* NAVIGATION BAR+ */}
+			<nav className='relative containe mx-auto p-8'>
+				<div className='flex items-center justify-between'>
+					<img src='src/img/logo.svg'></img>
+					<div className='hidden md:flex space-x-8 text-xl  text-navyBlue'>
+						<a href='#' className='hover:text-lightblue'>
+							Page 1
+						</a>
+						<a href='#' className='hover:text-lightblue'>
+							Page 2
+						</a>
+						<a href='#' className='hover:text-lightblue'>
+							Page 3
+						</a>
+						<a href='#' className='hover:text-lightblue'>
+							Page 4
+						</a>
+					</div>
+					<button class='hidden md:block bg-lightblue hover:bg-navyBlue text-white font-bold py-2 px-4 rounded-md'>
+						CLICKKK
+					</button>
+				</div>
+			</nav>
+		</>
+	);
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        navyBlue: "#05445E",
+        lightblue: "#189AB4",
+        bluegreen: "#75E6DA",
+        babyblue: "#D4F1F4",
+      },
+    },
   },
   plugins: [],
-}
-
+};
